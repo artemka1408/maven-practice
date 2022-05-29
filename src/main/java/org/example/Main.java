b87c24f0366663204ab03e7cbc00150da6191a82
student <student@itpark>
2022-05-29 17:55:25 +0300

    faker delete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ import com.github.javafaker.Faker;
 
 public class Main {
     public static void main(String[] args) {
-        Faker faker = new Faker();
-        String card = faker.finance().creditCard();
-        System.out.println("card = " + card);
+
     }
 }
